52ed95dcf6deafbf1c1789232c3a0026be1e1fe5
Aaditya <aadityagupta400@gmail.com>
2023-05-14 21:18:25 +0530

    sets up the bot

--- src/router.ts
+++ src/router.ts
@@ -1,3 +1,13 @@
+import {App} from '@slack/bolt';
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode: true, // add this
+  appToken: process.env.SLACK_APP_TOKEN,
+});
+
+
 app.message('ping', async ({message, say}) => {
   console.log(message);
   await say('pong');
